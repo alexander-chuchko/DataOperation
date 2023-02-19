9d59ce29cc32cffeaf9d8a9597241f4643cfa778
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-19 18:20:53 +0200

    Added MockService service

--- DataOperation/DataOperation/Services/LogService.cs
+++ DataOperation/DataOperation/Services/LogService.cs
@@ -1,20 +1,47 @@
 ﻿
 using DataOperation.Interfaces;
+using System.IO;
+using System.Reflection;
 
 namespace DataOperation.Services
 {
     public class LogService : ILogService
     {
+        public static string logFilePath = $@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\transactions.log";
         public string LogPath => throw new System.NotImplementedException();
 
         public string Read()
         {
-            throw new System.NotImplementedException();
+            string? readTransactions = null;
+
+            if (!File.Exists(logFilePath))
+            {
+                throw new System.InvalidOperationException();
+            }
+
+            using (var file = new StreamReader(logFilePath))
+            {
+                readTransactions = file.ReadToEnd();
+            }
+
+            return readTransactions?.Length > default(int) ?
+                readTransactions :
+                "There are no recorded transactions";
         }
 
         public void Write(string logInfo)
         {
-            throw new System.NotImplementedException();
+            if (!string.IsNullOrEmpty(logFilePath) && !string.IsNullOrEmpty(logInfo))
+            {
+                string? formattedString = string.Concat(logInfo, "\n");
+
+                bool isFile = File.Exists(logFilePath);
+
+                using (StreamWriter write = new StreamWriter(logFilePath, isFile))
+                {
+                    write.Write(formattedString);
+                }
+            }
         }
     }
 }
