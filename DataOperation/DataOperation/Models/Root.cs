3a7a65ae355b99bb8b3b226304588ef3c3462bf8
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-19 17:06:13 +0200

    Added Root model

---
+++ DataOperation/DataOperation/Models/Root.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DataOperation.Models
+{
+    public class Root
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("services")]
+        public List<Service> Services { get; set; }
+
+        [JsonProperty("total")]
+        public decimal total { get; set; }
+    }
+}
