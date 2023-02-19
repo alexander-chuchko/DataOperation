eeabaaa4a0ef7cc11fe0ad79de61ec5c52fafd8e
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-19 17:15:34 +0200

    Added ILogService service

---
+++ DataOperation/DataOperation/Interfaces/ILogService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DataOperation.Interfaces
+{
+    internal interface ILogService
+    {
+        string LogPath { get; }
+        void Write(string logInfo);
+        string Read();
+    }
+}
