5b678104d6582e89bae127ff730a4f9845155a98
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-19 17:02:51 +0200

    Added Payment model

---
+++ DataOperation/DataOperation/Models/Payment.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DataOperation.Models
+{
+    public class Payer
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("date")]
+        public decimal Payment { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("account_number")]
+        public long AccountNumber { get; set; }
+    }
+}
--- DataOperation/DataOperation/Program.cs
+++ DataOperation/DataOperation/Program.cs
@@ -2,7 +2,7 @@
 
 namespace DataOperation
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
