a61cd3f87eb09c0d4268369bfba778f39712ecf5
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-19 17:18:31 +0200

    Added LogService service

---
+++ DataOperation/DataOperation/Services/LogService.cs
@@ -0,0 +1,20 @@
+﻿
+using DataOperation.Interfaces;
+
+namespace DataOperation.Services
+{
+    public class LogService : ILogService
+    {
+        public string LogPath => throw new System.NotImplementedException();
+
+        public string Read()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(string logInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
