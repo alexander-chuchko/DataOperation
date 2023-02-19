5608161a2defb15738f8278a01bcc7fd9176683f
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-19 15:14:59 +0200

    Added DataOperation project

---
+++ DataOperation/DataOperation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
