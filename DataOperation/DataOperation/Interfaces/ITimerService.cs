8bc36f66d768e3de23a41055804688d28b651e85
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-27 12:43:37 +0200

    Added ITimerService service

---
+++ DataOperation/DataOperation/Interfaces/ITimerService.cs
@@ -0,0 +1,13 @@
+﻿using System.Timers;
+
+namespace DataOperation.Interfaces
+{
+    public interface ITimerService
+    {
+        event ElapsedEventHandler Elapsed;
+        double Interval { get; set; }
+        void Start();
+        void Stop();
+        void Dispose();
+    }
+}
