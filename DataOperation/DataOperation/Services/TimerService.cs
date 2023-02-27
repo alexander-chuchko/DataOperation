6767be485d8dee328e22639ee6e51fb93fee2fcb
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-02-27 12:46:07 +0200

    Added TimerService service

---
+++ DataOperation/DataOperation/Services/TimerService.cs
@@ -0,0 +1,46 @@
+﻿using DataOperation.Interfaces;
+using System.Timers;
+
+namespace DataOperation.Services
+{
+    public class TimerService : ITimerService
+    {
+        public event ElapsedEventHandler Elapsed;
+        private System.Timers.Timer aTimer;
+
+        public TimerService()
+        {
+            aTimer = new System.Timers.Timer();
+        }
+        public double Interval
+        {
+            get => aTimer.Interval;
+            set => aTimer.Interval = value;
+        }
+
+
+        public void Dispose()
+        {
+        }
+
+        public void Start()
+        {
+            aTimer.Elapsed += FireElapsedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Start();
+        }
+
+        public void Stop()
+        {
+            aTimer.Stop();
+        }
+
+        public void FireElapsedEvent(object? sender, ElapsedEventArgs e)
+        {
+            if (Elapsed != null)
+            {
+                Elapsed?.Invoke(this, null);
+            }
+        }
+    }
+}
